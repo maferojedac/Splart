bd9d59a2d121e3396dd7202e4c6cd7f510ac6155
JesusCiprian <0232134@up.edu.mx>
2024-05-15 22:04:07 -0600

    Tienda y Configuración - tarea terminada

---
+++ Assets/Scripts/Menus/SlidersControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlidersControl : MonoBehaviour
+{
+    public Slider MasterVolume;
+    public Slider SoundeffectsVolume;
+    public Slider MusicVolume;
+
+    public PlayerData playerData;
+
+    void Start()
+    {
+        // Cargar los valores guardados al iniciar el juego
+        MasterVolume.value = playerData.MasterVolume;
+        SoundeffectsVolume.value = playerData.SoundeffectsVolume;
+        MusicVolume.value = playerData.MusicVolume;
+    }
+
+    public void MasterVolumeChanged()
+    {
+        playerData.MasterVolume = MasterVolume.value;
+        // Aquí puedes cambiar el volumen del sonido general del juego
+    }
+
+    public void SoundeffectsVolumeChanged()
+    {
+        playerData.SoundeffectsVolume = SoundeffectsVolume.value;
+        // Aquí puedes cambiar el volumen de la música del juego
+    }
+
+    public void MusicVolumeChanged()
+    {
+        playerData.MusicVolume = MusicVolume.value;
+        // Aquí puedes cambiar el volumen de los efectos de sonido del juego
+    }
+}
---
+++ Assets/Scripts/Menus/StoreBuy.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreBuy : MonoBehaviour
+{
+    public Button BoosterSlow;
+    public Button BoosterThunder;
+    public Button BoosterClean;
+    public Button BoosterLife;
+    public Button Booster_AnyUpgrade;
+    public Button Booster_ScoreUpgrade;
+
+    public PlayerData playerData;
+
+    void Start()
+    {
+        playerData.BoosterSlow = 0;
+        playerData.BoosterThunder = 0;
+        playerData.BoosterClean = 0;
+        playerData.BoosterLife = 0;
+        playerData.Booster_AnyUpgrade = 0;
+        playerData.Booster_ScoreUpgrade = 0;
+
+
+        BoosterSlow.onClick.AddListener(BoosterSlowChanged);
+        BoosterThunder.onClick.AddListener(BoosterThunderChanged);
+        BoosterClean.onClick.AddListener(BoosterCleanChanged);
+        BoosterLife.onClick.AddListener(BoosterLifeChanged);
+        Booster_AnyUpgrade.onClick.AddListener(Booster_AnyUpgradeChanged);
+        Booster_ScoreUpgrade.onClick.AddListener(Booster_ScoreUpgradeChanged);
+    }
+
+    public void BoosterSlowChanged()
+    {
+        playerData.BoosterSlow++;
+    }
+
+    public void BoosterThunderChanged()
+    {
+        playerData.BoosterThunder++;
+    }
+
+    public void BoosterCleanChanged()
+    {
+        playerData.BoosterClean++;
+    }
+
+    public void BoosterLifeChanged()
+    {
+        playerData.BoosterLife++;
+    }
+
+    public void Booster_AnyUpgradeChanged()
+    {
+        playerData.Booster_AnyUpgrade++;
+    }
+
+    public void Booster_ScoreUpgradeChanged()
+    {
+        playerData.Booster_ScoreUpgrade++;
+    }
+}
